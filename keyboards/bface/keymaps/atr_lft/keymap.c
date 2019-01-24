626b1a4aa99f1ea849d9af021504e08555f69008
Allen Zhong <zhongbenli@pingcap.com>
2019-01-24 13:15:27 +0800

    move atr_lft layout from bfake keyboard to bface

--- keyboards/bface/keymaps/atr_lft/keymap.c
+++ keyboards/bface/keymaps/atr_lft/keymap.c
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 Luiz Ribeiro <luizribeiro@gmail.com>
+Copyright 2017 Sebastian Kaim <sebastian.kaim@sebb767.de>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -16,6 +16,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include QMK_KEYBOARD_H
+#include "quantum.h"
 
 #define _______ KC_TRNS
 
@@ -33,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |Ctrl|Gui |Alt |       Space            |Fn0|DEL|Lt |Dn |Rt |
     * `-----------------------------------------------------------'
     */
-    [0] = LAYOUT_all(
+    [0] = LAYOUT_lft(
         F(0),   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSPC,
         KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSLS,
         KC_CAPS,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,        KC_ENT,
@@ -53,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |    |Menu|    |                      |   |   |Home|PgDn|End|
     * `-----------------------------------------------------------'
     */
-    [1] = LAYOUT_all(
+    [1] = LAYOUT_lft(
         KC_GRV, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, _______,
         _______,KC_BTN1,KC_MS_U,KC_BTN2,_______,_______,_______,_______,KC_INS, _______,KC_PSCR,KC_SLCK,KC_PAUS,_______,
         _______,KC_MS_L,KC_MS_D,KC_MS_R,_______,_______,_______,_______,_______,_______,_______,_______,        _______,
