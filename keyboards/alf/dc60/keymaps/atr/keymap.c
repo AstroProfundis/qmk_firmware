20a3818d08ed882c222ee6c4e805354cbc0e781c
Allen Zhong <allen@moe.cat>
2020-01-22 21:12:30 +0800

    dc60: swap brightness up/down

--- keyboards/alf/dc60/keymaps/atr/keymap.c
+++ keyboards/alf/dc60/keymaps/atr/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [1] = LAYOUT_all(
       KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL, _______,
       _______, KC_BTN1, KC_MS_U, KC_BTN2, _______, _______, _______, _______, KC_INS,  _______, KC_PSCR, _______, _______, _______,
-      _______,          KC_MS_L, KC_MS_D, KC_MS_R, _______, _______, _______, _______, KC_BRIU, KC_BRID, _______, _______,          _______,
+      _______,          KC_MS_L, KC_MS_D, KC_MS_R, _______, _______, _______, _______, KC_BRID, KC_BRIU, _______, _______,          _______,
       _______, _______, KC_WH_U, KC_BTN3, KC_WH_D, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, _______, KC_PGUP, _______,
       MO(2),   _______, _______,                   _______, _______, _______,                   _______, _______, KC_HOME, KC_PGDN, KC_END
       ),
